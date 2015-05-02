481ee363ff3e14969b65699dcedeee25db77c81e
YanyingWang <yanyingwang1@gmail.com>
2015-05-02 13:15:52 +0800

    commented solarized theme code

--- init.el
+++ init.el
@@ -28,3 +28,11 @@
 (scroll-bar-mode -1)
 
 
+;;; solarized theme
+;(use-package solarized-theme
+;  :ensure t)
+
+;(require 'solarized)
+;(deftheme solarized-light "The light variant of the Solarized colour theme")
+;(create-solarized-theme 'light 'solarized-light)
+;(provide-theme 'solarized-light)
