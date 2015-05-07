ea25b07c4c6e617bfa72f391d612a306d9f2ae2b
YanyingWang <yanyingwang1@gmail.com>
2015-05-08 05:19:02 +0800

    faces & self

--- lisp/faces.el
+++ lisp/faces.el
@@ -14,12 +14,3 @@
 ;;; hide scroll bar
 (scroll-bar-mode -1)
 
-;;; Start in full-screen mode
-(custom-set-variables
- '(initial-frame-alist (quote ((fullscreen . maximized)))))
-
-
-;;; customize scratch message
-(setq initial-scratch-message
-      (concat ";; Happy hacking " (or user-login-name "") " - Emacs ♥ you!\n\n"))
-
--- lisp/self.el
+++ lisp/self.el
@@ -1,6 +1,19 @@
+;;; Start in full-screen mode
+(custom-set-variables
+ '(initial-frame-alist (quote ((fullscreen . maximized)))))
+
+
+
+;;; customize scratch message
+(setq initial-scratch-message
+      (concat ";; Happy hacking " (or user-login-name "") " - Emacs ♥ you!\n\n"))
+
+
+
 (setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
       backup-by-copying t    ; Don't delink hardlinks
       version-control t      ; Use version numbers on backups
       delete-old-versions t  ; Automatically delete excess backups
       kept-new-versions 20   ; how many of the newest versions to keep
       kept-old-versions 5)   ; and how many of the old
+
