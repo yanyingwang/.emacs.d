142b7e843acb8cbf2a0f4758efae95df4ebac2ba
YanyingWang <wangyanying@gmail.com>
2015-05-13 07:21:22 +0800

    moe-theme modify

--- lisp/themes.el
+++ lisp/themes.el
@@ -41,33 +41,40 @@
 (use-package moe-theme
   :ensure t)
 
+
 (require 'moe-theme)
+
+
 ;; Show highlighted buffer-id as decoration. (Default: nil)
 (setq moe-theme-highlight-buffer-id t)
+
 
 ;; Resize titles (optional).
 (setq moe-theme-resize-markdown-title '(1.5 1.4 1.3 1.2 1.0 1.0))
 (setq moe-theme-resize-org-title '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0))
 (setq moe-theme-resize-rst-title '(1.5 1.4 1.3 1.2 1.1 1.0))
 
-;; Resize titles
-(setq moe-theme-resize-markdown-title '(2.0 1.7 1.5 1.3 1.0 1.0))
-(setq moe-theme-resize-org-title '(2.2 1.8 1.6 1.4 1.2 1.0 1.0 1.0 1.0))
-(setq moe-theme-resize-rst-title '(2.0 1.7 1.5 1.3 1.1 1.0))
 
 ;; Choose a color for mode-line.(Default: blue)
-(moe-theme-set-color 'cyan)
+;;(moe-theme-set-color 'cyan)
 
 ;; Too Yellow Background in terminal?
 (setq moe-light-pure-white-background-in-terminal t)
 
+
+;;; paren
+;;(setq show-paren-style 'expression)
+
+
 ;; Finally, apply moe-theme now.
 ;; Choose what you like, (moe-light) or (moe-dark)
-(moe-light)
+;; (moe-light)
+
+(powerline-moe-theme)
 
 ;; automatically switch between moe-dark and moe-light according to the system time
 (require 'moe-theme-switcher)
 
-(moe-theme-set-color 'orange)
+;;(moe-theme-set-color 'orange)
 ;; (Available colors: blue, orange, green ,magenta, yellow, purple, red, cyan, w/b.)
 
