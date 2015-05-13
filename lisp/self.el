ace4e7871f9c807316236c700cf31714de00010f
YanyingWang <wangyanying@gmail.com>
2015-05-13 15:38:36 +0800

    hide welcome screen

--- lisp/self.el
+++ lisp/self.el
@@ -10,12 +10,18 @@
 
 
 
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
+
+
+
 (setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
       backup-by-copying t    ; Don't delink hardlinks
       version-control t      ; Use version numbers on backups
       delete-old-versions t  ; Automatically delete excess backups
       kept-new-versions 20   ; how many of the newest versions to keep
       kept-old-versions 5)   ; and how many of the old
+
 
 ;;; enable ShowParenMode
 (show-paren-mode 1)
