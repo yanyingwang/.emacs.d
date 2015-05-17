c2ce1b49f2e04567a0b588cf991bf8e9b2859c30
YanyingWang <wangyanying@gmail.com>
2015-05-18 01:37:33 +0800

    emacs temp file all to one dir

--- lisp/self.el
+++ lisp/self.el
@@ -15,12 +15,20 @@
 
 
 
-(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
-      backup-by-copying t    ; Don't delink hardlinks
-      version-control t      ; Use version numbers on backups
-      delete-old-versions t  ; Automatically delete excess backups
-      kept-new-versions 20   ; how many of the newest versions to keep
-      kept-old-versions 5)   ; and how many of the old
+(custom-set-variables
+  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
+  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
+
+;; create the autosave dir if necessary, since emacs won't.
+(make-directory "~/.emacs.d/autosaves/" t)
+(make-directory "~/.emacs.d/backups/" t)
+
+;;(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+;;backup-by-copying t    ; Don't delink hardlinks
+;;version-control t      ; Use version numbers on backups
+;;delete-old-versions t  ; Automatically delete excess backups
+;;kept-new-versions 20   ; how many of the newest versions to keep
+;;kept-old-versions 5)   ; and how many of the old
 
 
 ;;; enable ShowParenMode
