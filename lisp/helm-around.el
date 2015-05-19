526e9633d0ee61d59333e2601f80a58d2e759c55
YanyingWang <wangyanying@gmail.com>
2015-05-19 11:55:49 +0800

    (setq projectile-switch-project-action 'helm-projectile)

--- lisp/helm-around.el
+++ lisp/helm-around.el
@@ -9,9 +9,9 @@
 (helm-projectile-on)
 
 
-(setq projectile-switch-project-action 'helm-projectile-find-file)
+;;(setq projectile-switch-project-action 'helm-projectile-find-file)
 
-;;(setq projectile-switch-project-action 'helm-projectile)
+(setq projectile-switch-project-action 'helm-projectile)
 
 ;;(setq projectile-switch-project-action 'projectile-find-dir)
 ;;(setq projectile-find-dir-includes-top-level t)
