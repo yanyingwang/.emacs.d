27e076ccecc7ec33a2cdebd32e6bce11f182e3b3
YanyingWang <yanyingwang1@gmail.com>
2015-07-08 15:48:28 +0800

    auto-complete

---
+++ lisp/auto-complete.el
@@ -0,0 +1,10 @@
+(use-package auto-complete
+  :ensure t)
+(require 'auto-complete-config)
+(ac-config-default)
+(add-to-list 'ac-modes 'coffee-mode)
+(add-to-list 'ac-modes 'scss-mode)
+(add-to-list 'ac-modes 'sass-mode)
+(add-to-list 'ac-modes 'slim-mode)
+(add-to-list 'ac-modes 'yaml-mode)
+
