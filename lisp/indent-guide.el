c1dbc23de1ed234e700035932d1716162e9f902f
YanyingWang <yanyingwang1@gmail.com>
2015-08-25 13:23:06 +0800

    indent-guid color to be gray

--- lisp/indent-guide.el
+++ lisp/indent-guide.el
@@ -5,7 +5,8 @@
 (indent-guide-global-mode)
 
 ;;(set-face-foreground 'indent-guide-face "lightgray")
-(set-face-foreground 'indent-guide-face "dimgray")
+;;(set-face-foreground 'indent-guide-face "dimgray")
+(set-face-foreground 'indent-guide-face "gray")
 
 (setq indent-guide-delay 0.1)
 
