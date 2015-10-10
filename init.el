f8f1473384a7e6e7da9226fc41b81905e31308d8
YanyingWang <yanyingwang1@gmail.com>
2015-10-10 11:07:47 +0800

    rename lisp => init.d

--- init.el
+++ init.el
@@ -16,34 +16,34 @@
 
 
 ;;; load files
-(load "~/.emacs.d/lisp/locale")
-(load "~/.emacs.d/lisp/self")
-(load "~/.emacs.d/lisp/faces")
+(load "~/.emacs.d/init.d/locale")
+(load "~/.emacs.d/init.d/self")
+(load "~/.emacs.d/init.d/faces")
 
-(load "~/.emacs.d/lisp/langs-mode")
+(load "~/.emacs.d/init.d/langs-mode")
 
-(load "~/.emacs.d/lisp/powerline")
-(load "~/.emacs.d/lisp/golden-ratio")
+(load "~/.emacs.d/init.d/powerline")
+(load "~/.emacs.d/init.d/golden-ratio")
 
-(load "~/.emacs.d/lisp/auto-complete")
-(load "~/.emacs.d/lisp/aggressive-indent")
-;;(load "~/.emacs.d/lisp/auto-indent")
+(load "~/.emacs.d/init.d/auto-complete")
+(load "~/.emacs.d/init.d/aggressive-indent")
+;;(load "~/.emacs.d/init.d/auto-indent")
 
-(load "~/.emacs.d/lisp/smartparens")
-(load "~/.emacs.d/lisp/indent-guide")
-(load "~/.emacs.d/lisp/rainbow-delimiters")
+(load "~/.emacs.d/init.d/smartparens")
+(load "~/.emacs.d/init.d/indent-guide")
+(load "~/.emacs.d/init.d/rainbow-delimiters")
 
-;;(load "~/.emacs.d/lisp/highlight-indentation")
+;;(load "~/.emacs.d/init.d/highlight-indentation")
 
-(load "~/.emacs.d/lisp/helm")
-(load "~/.emacs.d/lisp/helm-projectile")
-(load "~/.emacs.d/lisp/helm-around")
+(load "~/.emacs.d/init.d/helm")
+(load "~/.emacs.d/init.d/helm-projectile")
+(load "~/.emacs.d/init.d/helm-around")
 
-(load "~/.emacs.d/lisp/magit")
+(load "~/.emacs.d/init.d/magit")
 
-(load "~/.emacs.d/lisp/ruby-world")
-(load "~/.emacs.d/lisp/slime")
+(load "~/.emacs.d/init.d/ruby-world")
+(load "~/.emacs.d/init.d/slime")
 
-(load "~/.emacs.d/lisp/moe-theme")
-;;(load "~/.emacs.d/lisp/gruvbox-theme")
-;;(load "~/.emacs.d/lisp/zenburn-theme")
+(load "~/.emacs.d/init.d/moe-theme")
+;;(load "~/.emacs.d/init.d/gruvbox-theme")
+;;(load "~/.emacs.d/init.d/zenburn-theme")
