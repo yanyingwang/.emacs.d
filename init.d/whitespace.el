2c8d2ae304000551d324e3f555a687a8f1a08c9a
YanyingWang <yanyingwang1@gmail.com>
2015-10-14 16:00:22 +0800

    delete whitespace

--- init.d/whitespace.el
+++ init.d/whitespace.el
@@ -1,18 +1,30 @@
 ;; cleaned up whitespace before saving
-(defun delete-trailing-whitespace-except-current-line ()
-  (interactive)
-  (let ((begin (line-beginning-position))
-        (end (line-end-position)))
-    (save-excursion
-      (when (< (point-min) begin)
-        (save-restriction
-          (narrow-to-region (point-min) (1- begin))
-          (delete-trailing-whitespace)))
-      (when (> (point-max) end)
-        (save-restriction
-          (narrow-to-region (1+ end) (point-max))
-          (delete-trailing-whitespace))))))
+;(defun delete-trailing-whitespace-except-current-line ()
+  ;(interactive)
+  ;(let ((begin (line-beginning-position))
+        ;(end (line-end-position)))
+    ;(save-excursion
+      ;(when (< (point-min) begin)
+        ;(save-restriction
+          ;(narrow-to-region (point-min) (1- begin))
+          ;(delete-trailing-whitespace)))
+      ;(when (> (point-max) end)
+        ;(save-restriction
+          ;(narrow-to-region (1+ end) (point-max))
+          ;(delete-trailing-whitespace))))))
 
+
+(defun delete-trailing-whitespace-except-current-line ()
+  "do delete-trailing-whitespace, except preserve whitespace of current line"
+  (interactive)
+  (let ((current-line (buffer-substring (line-beginning-position) (line-end-position)))
+        (backward (- (line-end-position) (point))))
+    (delete-trailing-whitespace)
+    (when (not (string-equal (buffer-substring (line-beginning-position) (line-end-position))
+                             current-line))
+      (delete-region (line-beginning-position) (line-end-position))
+      (insert current-line)
+      (backward-char backward))))
 
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace-except-current-line)
