f0d80489ef1f7f0ef22424051bed89fd9852cdbf
YanyingWang <yanyingwang1@gmail.com>
2015-12-03 16:38:00 +0800

    just one space

--- init/key-binding.el
+++ init/key-binding.el
@@ -1,5 +1,5 @@
 ;; for linux: X Windows
-(setq x-alt-keysym 'super)
-(setq x-super-keysym 'meta)
+;;(setq x-alt-keysym 'super)
+;;(setq x-super-keysym 'meta)
 
-;;(global-set-key (kbd "M-SPC") 'just-one-space)
+(global-set-key (kbd "M-SPC") 'just-one-space)
