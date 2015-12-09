6aa11329ae778ec1e1274b058912a9215eb26694
YanyingWang <yanyingwang1@gmail.com>
2015-12-09 15:58:52 +0800

    roswell

--- init/slime.el
+++ init/slime.el
@@ -23,3 +23,9 @@
 (define-key slime-repl-mode-map
   (read-kbd-macro paredit-backward-delete-key) nil))
   (add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit)
+
+
+;;roswell
+(load (expand-file-name "~/.roswell/impls/ALL/ALL/quicklisp/slime-helper.el"))
+  ;; Replace "sbcl" with the path to your implementation
+  (setq inferior-lisp-program "sbcl")
