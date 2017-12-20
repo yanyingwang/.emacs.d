3e77285fd3fda0d7b39fb8dd1be03187555a146a
YanyingWang <yanyingwang1@gmail.com>
2017-12-21 04:30:15 +0800

    magit with use-package function

--- init/magit.el
+++ init/magit.el
@@ -1,8 +1,5 @@
 ;;; magit
 (use-package magit
-  :ensure t)
-
-(setq magit-last-seen-setup-instructions "1.4.0")
-
-
-(global-set-key (kbd "C-x g") 'magit-status)
+  :ensure t
+  :bind (("C-x g" . magit-status))
+  :init (setq magit-last-seen-setup-instructions "1.4.0"))
