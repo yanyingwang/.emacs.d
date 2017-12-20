7b2448112fdf183c76c4fbbab4fb028fdc405189
YanyingWang <yanyingwang1@gmail.com>
2017-12-21 05:33:06 +0800

    geiser.el optimized

--- init/geiser.el
+++ init/geiser.el
@@ -1,18 +1,21 @@
 (use-package geiser
+  :init
+  (setq geiser-active-implementations '(chez))
+  (cond
+   ((string-equal system-type "windows-nt") ; Microsoft Windows
+    (progn
+      (message "Microsoft Windows")))
+   ((string-equal system-type "darwin") ; Mac OS X
+    (progn
+      (setq geiser-guile-binary "/usr/local/bin/guile")
+      (setq geiser-chez-binary "/usr/local/bin/chez")
+      (message "Mac OS X")))
+   ((string-equal system-type "gnu/linux") ; linux
+    (progn
+      (message "Linux"))))
+
+  (add-hook 'geiser-repl-mode-hook (lambda () (setq show-trailing-whitespace nil)))
+  (add-hook 'geiser-repl-mode-hook 'rainbow-delimiters-mode)
+  (add-hook 'geiser-repl-mode-hook (lambda () (paredit-mode +1)))
+
   :ensure t)
-
-(cond
- ((string-equal system-type "windows-nt") ; Microsoft Windows
-  (progn
-    (message "Microsoft Windows")))
- ((string-equal system-type "darwin") ; Mac OS X
-  (progn
-    (setq geiser-guile-binary "/usr/local/bin/guile")
-    (setq geiser-chez-binary "/usr/local/bin/chez")
-    (message "Mac OS X")))
- ((string-equal system-type "gnu/linux") ; linux
-  (progn
-    (message "Linux"))))
-
-
-;(setq geiser-active-implementations '(chez))
