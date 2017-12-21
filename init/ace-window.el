d254679f4127f2b1f4996707f1980daa5322ab56
YanyingWang <yanyingwang1@gmail.com>
2017-12-22 00:47:01 +0800

    use gruvbox-theme

---
+++ init/ace-window.el
@@ -0,0 +1,6 @@
+(use-package ace-window
+  ;:init
+  :config
+  (load-theme 'material t)
+  ;(load-theme 'material-light t)
+  :ensure t)
--- init/theme.el
+++ init/theme.el
@@ -1,14 +1,14 @@
-(use-package material-theme
-  ;:init 
-  :config
-  (load-theme 'material t)
-  ;(load-theme 'material-light t)
-  :ensure t)
+;(use-package material-theme
+;  ;:init
+;  :config
+;  (load-theme 'material t)
+;  ;(load-theme 'material-light t)
+;  :ensure t)
 
 ;(use-package zenburn-theme
-;  :init (load-theme 'zenburn t)
+;  :config (load-theme 'zenburn t)
 ;  :ensure t)
 
-;(use-package gruvbox-theme
-;  :init (load-theme 'gruvbox t)
-;  :ensure t)
+(use-package gruvbox-theme
+  :config (load-theme 'gruvbox t)
+  :ensure t)
