062cc1139ff0e1c5492fc65e10b7804bcbb1a82a
YanyingWang <yanyingwang1@gmail.com>
2017-12-22 01:45:43 +0800

    remove golden-radio and modify geiser and rm ueslessfile:gruvbox

--- init/geiser.el
+++ init/geiser.el
@@ -1,18 +1,20 @@
 (use-package geiser
   :init
-  (setq geiser-active-implementations '(chez))
   (cond
    ((string-equal system-type "windows-nt") ; Microsoft Windows
     (progn
       (message "Microsoft Windows")))
    ((string-equal system-type "darwin") ; Mac OS X
     (progn
-      (setq geiser-guile-binary "/usr/local/bin/guile")
       (setq geiser-chez-binary "/usr/local/bin/chez")
+      (setq geiser-guile-binary "/usr/local/bin/guile")
+      (setq geiser-mit-binary "/usr/local/bin/mit")
       (message "Mac OS X")))
    ((string-equal system-type "gnu/linux") ; linux
     (progn
       (message "Linux"))))
+  (setq geiser-active-implementations '(chez))
+  (setq geiser-repl-history-filename "~/.emacs.d/geiser-history")
 
   (add-hook 'geiser-repl-mode-hook (lambda () (setq show-trailing-whitespace nil)))
   (add-hook 'geiser-repl-mode-hook 'rainbow-delimiters-mode)
--- init/gruvbox.el
+++
@@ -1,6 +0,0 @@
-(use-package gruvbox-theme
-  :ensure t)
-
-;;(require 'moe-theme)
-
-(load-theme 'gruvbox t)
