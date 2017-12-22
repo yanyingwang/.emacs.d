ee6ec5fa647ef94e659520efc454d60ec85be85d
YanyingWang <yanyingwang1@gmail.com>
2017-12-23 02:05:24 +0800

    ace-window :bind syntax modify

--- init/ace-window.el
+++ init/ace-window.el
@@ -3,6 +3,6 @@
   (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
   (setq aw-background nil)
   ;:config
-  :bind (("M-[" . ace-window))
-  :bind (("M-]" . ace-window))
+  :bind (("M-[" . ace-window)
+         ("M-]" . ace-window))
   :ensure t)
