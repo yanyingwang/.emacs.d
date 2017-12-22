82720751641645d5d6cc37e83e3840ce160d6897
YanyingWang <yanyingwang1@gmail.com>
2017-12-23 02:05:38 +0800

    company-mode with tab key

--- init/company-mode.el
+++ init/company-mode.el
@@ -1,7 +1,9 @@
 (use-package company
   :init
   (add-hook 'after-init-hook 'global-company-mode)
+  (setq company-selection-wrap-around t)
   :config
   (push 'company-robe company-backends)
+  (define-key company-active-map (kbd "TAB") 'company-select-next)
+  (define-key company-active-map [tab] 'company-select-next)
   :ensure t)
-
