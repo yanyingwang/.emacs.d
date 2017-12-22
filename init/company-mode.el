435b3f12db4cd420b67a3ae2c70be50ee512e1e4
YanyingWang <yanyingwang1@gmail.com>
2017-12-23 02:55:30 +0800

    S-tab for company-select-previous

--- init/company-mode.el
+++ init/company-mode.el
@@ -4,6 +4,6 @@
   (setq company-selection-wrap-around t)
   :config
   (push 'company-robe company-backends)
-  (define-key company-active-map (kbd "TAB") 'company-select-next)
-  (define-key company-active-map [tab] 'company-select-next)
+  (define-key company-active-map (kbd "<tab>") 'company-select-next)
+  (define-key company-active-map (kbd "<backtab>") 'company-select-previous) ;<S-tab>
   :ensure t)
