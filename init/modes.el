7f4b3e6cdef252108feebcffcce7ee1969e7c11d
YanyingWang <yanyingwang1@gmail.com>
2018-01-30 15:00:00 +0800

    langs-mode => modes

---
+++ init/modes.el
@@ -0,0 +1,58 @@
+;;; web mode
+(use-package web-mode
+  :init
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-enable-auto-pairing t)
+  :config
+  (require 'web-mode)
+  :ensure t)
+
+(require 'web-mode)
+
+
+;;; scss mode
+(use-package scss-mode
+  :ensure t)
+
+
+(use-package less-css-mode
+  :ensure t)
+
+
+;;; slim mode
+(use-package slim-mode
+  :ensure t)
+
+(require 'slim-mode)
+
+;;; markdown mode
+(use-package markdown-mode
+  :ensure t)
+
+(require 'markdown-mode)
+
+
+;;; coffeescript mode
+(use-package coffee-mode
+  :ensure t)
+
+(custom-set-variables '(coffee-tab-width 2))
+
+
+;;; yaml
+(use-package yaml-mode
+  :ensure t)
+
+(add-hook 'yaml-mode-hook
+	  (lambda ()
+	    (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
