088fbfedc959ff8045a65a02a630c8ff73c0180c
YanyingWang <yanyingwang1@gmail.com>
2018-01-31 16:13:18 +0800

    modify js-mode

--- init/modes.el
+++ init/modes.el
@@ -56,3 +56,19 @@
 (add-hook 'yaml-mode-hook
 	  (lambda ()
 	    (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+
+
+
+
+;;; js2-mode
+(use-package js2-mode
+  :init
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+  (add-hook 'js-mode-hook 'js2-minor-mode)
+  (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+  (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . js2-jsx-mode))
+  (add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode))
+  (add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset 2)))
+  :config
+  :ensure t)
+
