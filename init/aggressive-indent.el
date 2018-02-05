37ea2dceeb29451676deb2f6f5cc8b734546e3dc
Yanying Wang <yanyingwang1@gmail.com>
2018-02-05 19:21:04 +0800

    add aggressive excluded slim haml ruby

--- init/aggressive-indent.el
+++ init/aggressive-indent.el
@@ -2,5 +2,8 @@
 (use-package aggressive-indent
   :config
   (add-to-list 'aggressive-indent-excluded-modes 'web-mode)    ; exclude html
+  (add-to-list 'aggressive-indent-excluded-modes 'slim-mode)
+  (add-to-list 'aggressive-indent-excluded-modes 'haml-mode)
+  (add-to-list 'aggressive-indent-excluded-modes 'ruby-mode)
   (global-aggressive-indent-mode 1)
   :ensure t)
