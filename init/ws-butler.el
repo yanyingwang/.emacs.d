96dda555006077a873ba36d7703770975f53956e
Yanying Wang <yanyingwang1@gmail.com>
2018-02-05 18:18:17 +0800

    remove useless code

--- init/ws-butler.el
+++ init/ws-butler.el
@@ -1,16 +1,4 @@
 (use-package ws-butler
+  :config
+  (add-hook 'find-file-hook #'ws-butler-global-mode)
   :ensure t)
-
-
-;; autoload ws-butler on file open
-(add-hook 'find-file-hook #'ws-butler-global-mode)
-(setq require-final-newline t)
-
-;; ws-butler also load highlight-changes-mode
-;(with-eval-after-load 'ws-butler
-  ;; (diminish 'ws-butler-global-mode)
-  ;(diminish 'ws-butler-mode " β"))
-
-;(add-hook 'highlight-changes-mode-hook
-  ;(lambda ()
-    ;(diminish 'highlight-changes-mode)))
