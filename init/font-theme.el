e74f9a5086c3d304820dc30bc78c420eed853911
Yanying Wang <yanyingwang1@gmail.com>
2018-02-08 19:47:47 +0800

    only use bold and gruvbox-light-theme

--- init/font-theme.el
+++ init/font-theme.el
@@ -26,13 +26,13 @@
 
 (use-package gruvbox-theme
   :config
-  (load-theme 'gruvbox t)
-;;  ;(load-theme 'gruvbox-dark-medium t)
-;;  ;(load-theme 'gruvbox-dark-soft  t)
-;;  ;(load-theme 'gruvbox-dark-hard  t)
-;;  ;(load-theme 'gruvbox-light-medium t)
-;;  ;(load-theme 'gruvbox-light-soft  t)
-;;  ;(load-theme 'gruvbox-light-hard  t)
+  ;;(load-theme 'gruvbox t)
+  ;;  (load-theme 'gruvbox-dark-medium t)
+  ;;  (load-theme 'gruvbox-dark-soft  t)
+  ;;  (load-theme 'gruvbox-dark-hard  t)
+  (load-theme 'gruvbox-light-medium t)
+  ;;(load-theme 'gruvbox-light-soft  t)
+  ;;(load-theme 'gruvbox-light-hard  t)
   :ensure t)
 
 
@@ -43,24 +43,21 @@
 ;;  (load-theme 'material t)
 ;;  ;(load-theme 'material-light t)
 ;;  :ensure t)
-;
+                                        ;
 ;;(use-package zenburn-theme
 ;;  :config (load-theme 'zenburn t)
 ;;  :ensure t)
 
 
 
-
-(use-package theme-changer
-  :init
-  (setq calendar-location-name "Shanghai, China")
-  (setq calendar-latitude 31.22)
-  (setq calendar-longitude 121.48)
-  :config
-  (require 'theme-changer)
-  (change-theme 'gruvbox-light-medium 'gruvbox-dark-medium)
-  :ensure t)
-
-
-
-
+;;
+;;(use-package theme-changer
+;;  :init
+;;  (setq calendar-location-name "Shanghai, China")
+;;  (setq calendar-latitude 31.22)
+;;  (setq calendar-longitude 121.48)
+;;  :config
+;;  (require 'theme-changer)
+;;  (change-theme 'gruvbox-light-medium 'gruvbox-dark-medium)
+;;  :ensure t)
+;;
