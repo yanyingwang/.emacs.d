e89b035b9ffd3d2e4f09a8be0919aa4ffbb66c07
Yanying Wang <yanyingwang1@gmail.com>
2018-02-12 15:49:01 +0800

    rename

---
+++ init/windows-buffer.el
@@ -0,0 +1,9 @@
+(use-package ace-window
+  :init
+  (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+  (setq aw-background nil)
+  :config
+  :bind (("M-[" . ace-window)
+         ("M-]" . ace-window))
+  :ensure t)
+
--- init/windows.el
+++
@@ -1,9 +0,0 @@
-(use-package ace-window
-  :init
-  (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
-  (setq aw-background nil)
-  ;;:config
-  :bind (("M-[" . ace-window)
-         ("M-]" . ace-window))
-  :ensure t)
-
