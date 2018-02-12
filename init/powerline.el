67b72f91762c54052f97220ca2d982c7a7146315
Yanying Wang <yanyingwang1@gmail.com>
2018-02-12 15:48:24 +0800

    remove useless code of powerline

--- init/powerline.el
+++ init/powerline.el
@@ -1,21 +1,10 @@
-;;;;;; powerline
-;;(use-package powerline
-;;  :config
-;;  (require 'powerline)
-;;  (powerline-default-theme)
-;;  ;;(setq powerline-arrow-shape 'arrow)   ; the default
-;;  (setq powerline-arrow-shape 'curve)   ; give your mode-line curves
-;;  ;;(setq powerline-arrow-shape 'arrow14) ; best for small fonts
-;;  :ensure t)
-;;
-
-;;; micgoline
 (use-package micgoline
   :config
   (require 'micgoline)
   (setq ns-use-srgb-colorspace nil)
   (setq powerline-default-separator 'slant)
   :ensure t)
+
 
 ;;alternate
 ;;arrow
@@ -36,16 +25,13 @@
 ;;nil
 
 
-
-;;; telephone-line
-;;
-;;(use-package telephone-line
+;;;;;; powerline
+;;(use-package powerline
 ;;  :config
-;;  (require 'telephone-line)
-;;  (telephone-line-mode 1)
-;;  (setq telephone-line-primary-right-separator 'telephone-line-identity-left
-;;        telephone-line-secondary-right-separator 'telephone-line-identity-hollow-left)
-;;  (setq telephone-line-height 24
-;;        telephone-line-evil-use-short-tag t)
+;;  (require 'powerline)
+;;  (powerline-default-theme)
+;;  ;;(setq powerline-arrow-shape 'arrow)   ; the default
+;;  (setq powerline-arrow-shape 'curve)   ; give your mode-line curves
+;;  ;;(setq powerline-arrow-shape 'arrow14) ; best for small fonts
 ;;  :ensure t)
 ;;
