1600ae876a6e47329f85b82f9a233e026c71180f
Yanying Wang <yanyingwang1@gmail.com>
2018-02-12 16:28:04 +0800

    face modify

--- init/face.el
+++ init/face.el
@@ -1,8 +1,10 @@
-(global-linum-mode t)                   ;line number
-;;(setq linum-format "%d ")
+(global-linum-mode 1)                   ;line number
+(setq linum-format "%d ")
 (tool-bar-mode -1)                      ;tool bar
 (scroll-bar-mode 1)                     ;scroll bar
 (menu-bar-mode 1)                       ;menu bar
 
 
 
+(display-time-mode 1)                   ;display time
+(setq display-time-format "%Y-%m-%d %H:%M")
