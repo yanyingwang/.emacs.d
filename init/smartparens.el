baf1fc4d5a823edcb818deff0532cefef862725e
Yanying Wang <yanyingwang1@gmail.com>
2018-02-13 15:18:59 +0800

    fix smartparens config not work

--- init/smartparens.el
+++ init/smartparens.el
@@ -1,19 +1,14 @@
-(use-package smartparens
-  :bind (("M-D" . sp-unwrap-sexp))
-  :config
-  (require 'smartparens-config)
-  ;;(add-hook 'js-mode-hook #'smartparens-mode)
-  (smartparens-global-mode t)
-  (show-smartparens-global-mode t)
-  :ensure t)
+e(use-package smartparens
+   :bind (("M-D" . sp-unwrap-sexp))
+   :config
+   :ensure t)
 
-;;
-;;(use-package smartparens-config
-;;  :ensure smartparens
-;;  :config
-;;  (progn
-;;    (show-smartparens-global-mode t)))
-;;
-;;(add-hook 'prog-mode-hook 'smartparens-global-mode)
-;;(add-hook 'markdown-mode-hook 'turn-on-smartparens-strict-mode)
-;;
+
+(use-package smartparens-config
+  :ensure smartparens
+  :config
+  (progn
+    (show-smartparens-global-mode t)))
+
+(add-hook 'prog-mode-hook 'smartparens-global-mode)
+(add-hook 'markdown-mode-hook 'turn-on-smartparens-strict-mode)
