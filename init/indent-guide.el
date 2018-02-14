9ee2fb6477468cea0c9cb1ccd15910c68a60c653
Yanying Wang <yanyingwang1@gmail.com>
2018-02-14 11:49:09 +0800

    modify indent guid

--- init/indent-guide.el
+++ init/indent-guide.el
@@ -1,11 +1,13 @@
 ;;; indent-guide
 (use-package indent-guide
-  :config
+  :init
   (set-face-foreground 'indent-guide-face "dimgray")
   ;;(set-face-foreground 'indent-guide-face "lightgray")
   ;;(set-face-foreground 'indent-guide-face "gray")
   (setq indent-guide-delay 0.5)
   (setq indent-guide-recursive t)
   (setq indent-guide-char "¦")            ;  ¦ │ ┆ ┋ ┋
+  :config
+  (require 'indent-guide)
   (indent-guide-global-mode)
   :ensure t)
