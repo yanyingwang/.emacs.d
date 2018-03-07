5ea26b03129abdc94a4979659623896aac91a5fe
Yanying Wang <yanyingwang1@gmail.com>
2018-03-07 16:16:57 +0800

    fly company with all features

--- init/company.el
+++ init/company.el
@@ -3,13 +3,13 @@
   (add-hook 'after-init-hook 'global-company-mode)
   (setq company-selection-wrap-around t)
   (setq company-idle-delay 0)
-  ;;(setq company-auto-select-first-candidate nil)
-  ;;(setq company-auto-complete t)   ; more key like space C-f use select suggestion
-  ;;(setq company-auto-complete 'company-explicit-action-p) ; auto-complete enable only when used select-next at least one time
+  (setq company-auto-select-first-candidate nil)
+  (setq company-auto-complete t)   ; more key like space C-f use select suggestion
+  (setq company-auto-complete 'company-explicit-action-p) ; auto-complete enable only when used select-next at least one time
   :config
   (push 'company-robe company-backends)
-  ;;(define-key company-active-map (kbd "<tab>") 'company-select-next)
-  ;;(define-key company-active-map (kbd "<backtab>") 'company-select-previous) ;<S-tab>
-  ;;(define-key company-active-map (kbd "C-n") 'company-select-next)
-  ;;(define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (define-key company-active-map (kbd "<tab>") 'company-select-next)
+  (define-key company-active-map (kbd "<backtab>") 'company-select-previous) ;<S-tab>
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
   :ensure t)
