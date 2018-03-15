f5615709e858aeb2e356998899fe6f081e6d5586
Yanying Wang <yanyingwang1@gmail.com>
2018-03-15 12:53:03 +0800

    add expand-region and rename to content-edit

---
+++ init/content-edit.el
@@ -0,0 +1,17 @@
+(use-package multiple-cursors
+  :bind (("C-S-c C-S-c" . mc/edit-lines)
+         ("C-S-c C-S-c" . 'mc/edit-lines)
+         ("C->" . 'mc/mark-next-like-this)
+         ("C-<" . 'mc/mark-previous-like-this)
+         ("C-c C-<" . 'mc/mark-all-like-this)
+         ("C-S-<mouse-1>" . 'mc/add-cursor-on-click))
+  :init
+  :config
+  :ensure t)
+
+
+(use-package expand-region
+  :bind (("C-=" . 'er/expand-region))
+  :init
+  :config
+  :ensure t)
