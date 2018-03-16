7ab0221e77d53a93273d1176fe0545f883ceacb7
Yanying Wang <yanyingwang1@gmail.com>
2018-03-16 16:20:45 +0800

    return with m-cursors

--- init/content-edit.el
+++ init/content-edit.el
@@ -1,14 +1,17 @@
 (use-package multiple-cursors
+  :defer 2
   :bind (("C-S-c C-S-c" . mc/edit-lines)
          ("C-S-c C-S-c" . 'mc/edit-lines)
          ("C->" . 'mc/mark-next-like-this)
          ("C-<" . 'mc/mark-previous-like-this)
          ("C-c C-<" . 'mc/mark-all-like-this)
+         ("C-;" . 'mc/mark-next-like-this-word)
+         ("C-:" . 'mc/mark-next-like-this-symbol)
+         (:map mc/keymap ("<return>" . nil))
          ("C-S-<mouse-1>" . 'mc/add-cursor-on-click))
   :init
   :config
   :ensure t)
-(define-key mc/keymap (kbd "<return>") nil)
 
 
 (use-package expand-region
