87f9ea221e7a2c3f673da218aa9f6358d13258db
Yanying Wang <yanyingwang1@gmail.com>
2018-03-16 17:01:38 +0800

    add iedit and region-bindings-mode

--- init/content-edit.el
+++ init/content-edit.el
@@ -5,8 +5,6 @@
          ("C->" . 'mc/mark-next-like-this)
          ("C-<" . 'mc/mark-previous-like-this)
          ("C-c C-<" . 'mc/mark-all-like-this)
-         ("C-;" . 'mc/mark-next-like-this-word)
-         ("C-:" . 'mc/mark-next-like-this-symbol)
          (:map mc/keymap ("<return>" . nil))
          ("C-S-<mouse-1>" . 'mc/add-cursor-on-click))
   :init
@@ -14,8 +12,29 @@
   :ensure t)
 
 
+(use-package region-bindings-mode
+  :bind ((:map region-bindings-mode-map
+               ("a" . 'mc/mark-all-like-this)
+               ("p" . 'mc/mark-previous-like-this)
+               ("n" . 'mc/mark-next-like-this)
+               ("m" . 'mc/mark-more-like-this-extended)))
+  :init
+  (setq region-bindings-mode-disable-predicates ((lambda () buffer-read-only)))
+  :config
+  (require 'region-bindings-mode)
+  (region-bindings-mode-enable)
+  :ensure t)
+
+
 (use-package expand-region
   :bind (("C-=" . 'er/expand-region))
   :init
   :config
+  :ensure t)
+
+
+(use-package iedit
+  :init
+  :config
+  (require 'iedit)
   :ensure t)
