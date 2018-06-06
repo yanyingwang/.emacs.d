a6053721b20ade71bb1e2f2ee10ac85311730744
Yanying Wang <yanyingwang1@gmail.com>
2018-06-06 17:39:40 +0800

    awesome rainbow delimiters custom

--- init/rainbow-delimiters.el
+++ init/rainbow-delimiters.el
@@ -2,3 +2,19 @@
   :ensure t)
 
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "#458588"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "#b16286"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "#8ec07c"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "#d65d0e"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "dark magenta"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "dark cyan"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "DeepPink2"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "forest green"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "OrangeRed")))))
+
