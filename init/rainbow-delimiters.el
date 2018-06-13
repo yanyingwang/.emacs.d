65a697df55e1630ab8be3858bf294b20aac0870e
Yanying Wang <yanyingwang1@gmail.com>
2018-06-13 10:41:50 +0800

    awesome rainbow delimiters custom

--- init/rainbow-delimiters.el
+++ init/rainbow-delimiters.el
@@ -12,9 +12,10 @@
  '(rainbow-delimiters-depth-2-face ((t (:foreground "#b16286"))))
  '(rainbow-delimiters-depth-3-face ((t (:foreground "#8ec07c"))))
  '(rainbow-delimiters-depth-4-face ((t (:foreground "#d65d0e"))))
+
  '(rainbow-delimiters-depth-5-face ((t (:foreground "dark magenta"))))
+
  '(rainbow-delimiters-depth-6-face ((t (:foreground "dark cyan"))))
  '(rainbow-delimiters-depth-7-face ((t (:foreground "DeepPink2"))))
  '(rainbow-delimiters-depth-8-face ((t (:foreground "forest green"))))
- '(rainbow-delimiters-depth-9-face ((t (:foreground "OrangeRed")))))
-
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "dark red")))))
