ee1d0a62bb60e43511294f454813a695a62526cc
Yanying Wang <yanyingwang1@gmail.com>
2018-07-03 18:17:13 +0800

    update geiser config

--- init/geiser.el
+++ init/geiser.el
@@ -1,27 +1,24 @@
 (use-package geiser
   :init
   (cond
-   ((string-equal system-type "darwin") ; Mac OS X
+   ((string-equal system-type "darwin")
     (progn
       (setq geiser-chez-binary "/usr/local/bin/chez")
       (setq geiser-guile-binary "/usr/local/bin/guile")
       (setq geiser-mit-binary "/usr/local/bin/mit")
       (setq geiser-chicken-binary "/usr/local/bin/csi")
       (message "Mac OS X")))
-   ((string-equal system-type "windows-nt") ; Microsoft Windows
+   ((string-equal system-type "windows-nt")
     (progn
       (message "Microsoft Windows")))
-   ((string-equal system-type "gnu/linux") ; linux
+   ((string-equal system-type "gnu/linux")
     (progn
       (message "Linux"))))
-  (setq geiser-active-implementations '(chez))
+  ;;(setq geiser-active-implementations '(chez))
   (setq geiser-repl-history-filename "~/.emacs.d/geiser-history")
-
+  (setq geiser-repl-use-other-window nil)
   (add-hook 'geiser-repl-mode-hook (lambda () (setq show-trailing-whitespace nil)))
   (add-hook 'geiser-repl-mode-hook 'rainbow-delimiters-mode)
-  ;;(add-hook 'geiser-repl-mode-hook (lambda () (paredit-mode +1)))
-
   :ensure t)
-
 
 (setq scheme-program-name "chez")
