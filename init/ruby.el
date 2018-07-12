dc583fc4e7718cab5eb1330482d242d15c09f7d1
Yanying Wang <yanyingwang1@gmail.com>
2018-07-12 14:26:39 +0800

    change back to ruby-mode

--- init/ruby.el
+++ init/ruby.el
@@ -12,27 +12,6 @@
   (rvm-use-default)
   :ensure t)
 
-(use-package enh-ruby-mode
-  :init
-  (add-to-list 'auto-mode-alist
-               '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
-  (remove-hook 'enh-ruby-mode-hook 'erm-define-faces)
-  (setq enh-ruby-indent-level 2)
-  ;;(setq enh-ruby-comment-column 32)
-  ;;(setq enh-ruby-bounce-deep-indent nil)
-  ;;(setq enh-ruby-hanging-indent-level 2)
-
-  ;;(setq enh-ruby-hanging-brace-indent-level 2)
-  (setq enh-ruby-hanging-brace-deep-indent-level 1)
-
-  ;;(setq enh-ruby-deep-indent-paren t)
-  ;;(setq enh-ruby-hanging-paren-deep-indent-level 0)
-  ;;(setq enh-ruby-hanging-paren-indent-level 2)
-  :config
-  ;;(define-key company-active-map (kbd "C-n") 'company-select-next)
-  ;;(define-key company-active-map (kbd "C-p") 'company-select-previous)
-  :ensure t)
-
 (use-package robe
   :init
   (add-hook 'ruby-mode-hook 'robe-mode)
