c0580cf03ae92039de118c2c1c53c5afa7a94d6d
Yanying Wang <yanyingwang1@gmail.com>
2018-07-20 19:01:54 +0800

    backup-directory.el

---
+++ init/backup-directory.el
@@ -0,0 +1,16 @@
+;; https://www.emacswiki.org/emacs/BackupDirectory
+
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+(message "Deleting old backup files...")
+(let ((week (* 60 60 24 7))
+      (current (float-time (current-time))))
+  (dolist (file (directory-files temporary-file-directory t))
+    (when (and (backup-file-name-p file)
+               (> (- current (float-time (fifth (file-attributes file))))
+                  week))
+      (message "%s" file)
+      (delete-file file))))
