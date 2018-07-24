ed3d43780ee73bdf81c12afdf7f158382bdf54ae
Yanying Wang <yanyingwang1@gmail.com>
2018-07-24 18:12:59 +0800

    buffer switch global keybinding

--- init/self/self-keybinding.el
+++ init/self/self-keybinding.el
@@ -3,25 +3,22 @@
 (global-unset-key (kbd "C-x C-z"))
 (global-unset-key (kbd "C-x c"))
 
-
 ;; enable C-x C-u & C-x C-l
 ;;(put 'upcase-region 'disabled nil)
 ;;(put 'downcase-region 'disabled nil)
 
-
-
 ;; other windows
 (defun previous-other-window () (interactive) (other-window -1))
-
 
 (global-set-key (kbd "M-[") 'other-window)
 (global-set-key (kbd "<C-tab>") 'other-window)
 (global-set-key (kbd "M-]") 'previous-other-window)
 (global-set-key (kbd "<C-S-tab>") 'previous-other-window)
 (global-set-key (kbd "<M-return>") 'toggle-frame-fullscreen)
+(global-set-key (kbd "<M-escape>") 'next-buffer)
+(global-set-key (kbd "<M-S-escape>") 'previous-buffer)
 
-
-;; customize for system conflict
+;; key customize for system conflict
 (cond ((string-equal system-type "windows-nt")
        (progn
          (message "Key-binding set for Windows")))
