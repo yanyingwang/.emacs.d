529087236b2f11ce96bc99eda57a67562bd5ebe8
Yanying Wang <yanyingwang1@gmail.com>
2018-07-31 18:18:59 +0800

    a new theme

--- init/z-font-theme.el
+++ init/z-font-theme.el
@@ -30,6 +30,32 @@
   :ensure t :defer)
 
 
+;;
+;;(use-package doom-themes
+;;  :init
+;;  (require 'doom-themes)
+;;  ;; Global settings (defaults)
+;;  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+;;        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+;;  :config
+;;  ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
+;;  ;; may have their own settings.
+;;  (load-theme 'doom-one t)
+;;
+;;  ;; Enable flashing mode-line on errors
+;;  (doom-themes-visual-bell-config)
+;;
+;;  ;; Enable custom neotree theme (all-the-icons must be installed!)
+;;  (doom-themes-neotree-config)
+;;  ;; or for treemacs users
+;;  (doom-themes-treemacs-config)
+;;
+;;  ;; Corrects (and improves) org-mode's native fontification.
+;;  (doom-themes-org-config)
+;;  :ensure t)
+;;
+
+
 (use-package circadian
   :ensure t
   :config
@@ -57,4 +83,3 @@
                     (set-frame-font "agave-14:book" nil t)
                     (set-face-foreground 'indent-guide-face "dimgray"))))))
   (circadian-setup))
-
