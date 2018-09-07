a899fbfb24ec8e17ee8660a250d16971401a7210
Yanying Wang <yanyingwang1@gmail.com>
2018-09-07 15:23:46 +0800

    add browse-at-remote

--- init/manage/git.el
+++ init/manage/git.el
@@ -1,5 +1,8 @@
-;;; magit
 (use-package magit
   :bind (("C-x g" . magit-status))
   :init (setq magit-last-seen-setup-instructions "1.4.0")
+  :ensure t)
+
+(use-package browse-at-remote
+  :bind (("C-c g g" . browse-at-remote))
   :ensure t)
