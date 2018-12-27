133bf6201b8ec41dacacd62a1c7b26208dc5b086
Yanying Wang <yanyingwang1@gmail.com>
2018-12-27 11:25:55 +0800

    bing-dict

--- init/tool/bing-dict.el
+++ init/tool/bing-dict.el
@@ -3,7 +3,7 @@
   (require 'bing-dict)
   (global-set-key (kbd "C-c d") 'bing-dict-brief)
   (setq bing-dict-show-thesaurus 'both) ;nil, 'synonym, 'antonym or 'both
-  (setq bing-dict-pronunciation-style 'uk)
+  (setq bing-dict-pronunciation-style 'us)
   (setq bing-dict-save-search-result t)
   ;;(setq bing-dict-org-file "/path/to/your_vocabulary.org")
   :ensure t)
