71d3ddeae214e534b0c3eb37c02242d2a2fcce68
Yanying Wang <yanyingwang1@gmail.com>
2019-01-07 16:55:09 +0800

    weather

---
+++ init/tool/weather.el
@@ -0,0 +1,11 @@
+
+
+(use-package wttrin
+  :ensure t
+  :commands (wttrin)
+  :init
+  (setq wttrin-default-cities '("Shanghai"
+                                "Zhengzhou"
+                                "California")))
+
+
