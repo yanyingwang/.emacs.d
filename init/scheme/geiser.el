c9b81e0690d6dfed217f4b20625b73e8aa9c28ef
Yanying Wang <yanyingwang1@gmail.com>
2019-01-18 11:32:03 +0800

    racket path

--- init/scheme/geiser.el
+++ init/scheme/geiser.el
@@ -11,6 +11,7 @@
    ((string-equal system-type "darwin")
     (progn
       (setq geiser-chez-binary "/usr/local/bin/chez")
+      (setq geiser-racket-binary "/Applications/Racket v7.1/bin/racket")
       (setq geiser-guile-binary "/usr/local/bin/guile")
       (setq geiser-mit-binary "/usr/local/bin/mit")
       (setq geiser-chicken-binary "/usr/local/bin/csi")
@@ -24,6 +25,7 @@
   ;;(setq geiser-active-implementations '(chez))
   (setq geiser-repl-history-filename "~/.emacs.d/geiser-history")
   (setq geiser-repl-use-other-window nil)
+  ;;(setq geiser-repl-startup-time 20000)
   (add-hook 'geiser-repl-mode-hook (lambda () (setq show-trailing-whitespace nil)))
   (add-hook 'geiser-repl-mode-hook 'rainbow-delimiters-mode)
   (add-hook 'geiser-repl-mode-hook 'smartparens-strict-mode)
