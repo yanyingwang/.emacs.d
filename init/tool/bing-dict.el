9b88a6938ed10b656deb362ae7c71a46ec85374f
Yanying Wang <yanyingwang1@gmail.com>
2019-02-15 12:43:07 +0800

    youdao dict

--- init/tool/bing-dict.el
+++ init/tool/bing-dict.el
@@ -5,5 +5,14 @@
   (setq bing-dict-show-thesaurus 'both) ;nil, 'synonym, 'antonym or 'both
   (setq bing-dict-pronunciation-style 'us)
   (setq bing-dict-save-search-result t)
+  (setq bing-dict-cache-auto-save t)
   ;;(setq bing-dict-org-file "/path/to/your_vocabulary.org")
+  :ensure t)
+
+
+
+(use-package youdao-dictionary
+  :config
+  (setq url-automatic-caching t)
+  (global-set-key (kbd "C-c y") 'youdao-dictionary-search-at-point)
   :ensure t)
