6adc8a60c52c4e282939a1ef8d3b2449a6c7f98e
Yanying Wang <yanyingwang1@gmail.com>
2019-02-26 16:21:36 +0800

    golden-raio

--- init.el
+++ init.el
@@ -31,3 +31,25 @@
 (load-directory "~/.emacs.d/init")
 (load-directory "~/.emacs.d/init/face")
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (golden-ratio youdao-dictionary yasnippet yaml-mode wttrin ws-butler web-mode use-package undo-tree smartparens slime slim-mode skewer-mode scss-mode rvm ruby-refactor robe region-bindings-mode rainbow-delimiters projectile-rails pinyin-search phi-search multiple-cursors micgoline markdown-mode magit indium indent-guide helm-themes helm-projectile helm-descbinds haml-mode gruvbox-theme github-stars geiser expand-region exec-path-from-shell emr diff-hl coffee-mode circadian chicken-scheme cal-china-x browse-at-remote bing-dict auto-package-update aggressive-indent ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "#458588"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "#b16286"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "#8ec07c"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "#d65d0e"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "dark magenta"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "dark cyan"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "DeepPink2"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "forest green"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "dark red")))))
---
+++ init/face/golden-ratio.el
@@ -0,0 +1,46 @@
+(use-package golden-ratio
+  :diminish golden-ratio-mode
+  :init
+  (setq golden-ratio-adjust-factor .9
+        golden-ratio-wide-adjust-factor .9)
+  :config
+  (golden-ratio-mode 1)
+  :ensure t)
+
+;; ediff
+(eval-after-load "golden-ratio"
+  '(progn
+     (add-to-list 'golden-ratio-exclude-modes "ediff-mode")
+     (add-to-list 'golden-ratio-inhibit-functions 'pl/ediff-comparison-buffer-p)))
+
+(defun pl/ediff-comparison-buffer-p ()
+  (and (boundp 'ediff-this-buffer-ediff-sessions)
+       ediff-this-buffer-ediff-sessions))
+
+;; The version which also called balance-windows at this point looked
+;; a bit broken, but could probably be replaced with:
+;;
+;; (defun pl/ediff-comparison-buffer-p ()
+;;   (and (boundp 'ediff-this-buffer-ediff-sessions)
+;;        ediff-this-buffer-ediff-sessions
+;;        (prog1 t (balance-windows))))
+;;
+;; However I think the following has the desired effect, and without
+;; messing with the ediff control buffer:
+;;
+(add-hook 'ediff-startup-hook 'my-ediff-startup-hook)
+
+(defun my-ediff-startup-hook ()
+  "Workaround to balance the ediff windows when golden-ratio is enabled."
+  ;; There's probably a better way to do it.
+  (ediff-toggle-split)
+  (ediff-toggle-split))
+
+
+;; helm
+(eval-after-load "golden-ratio"
+  '(add-to-list 'golden-ratio-inhibit-functions 'pl/helm-alive-p))
+
+(defun pl/helm-alive-p ()
+  (and (boundp 'helm-alive-p)
+       (symbol-value 'helm-alive-p)))
