e1a013b94fffa65592fc1fcc048a20d02e248a41
Yanying Wang <yanyingwang1@gmail.com>
2019-02-27 16:00:48 +0800

    dim-buffers

--- init/face/dim-buffers.el
+++ init/face/dim-buffers.el
@@ -1,8 +1,17 @@
-
 (use-package auto-dim-other-buffers
   :init
   :config
-   (add-hook 'after-init-hook (lambda ()
+  (add-hook 'after-init-hook (lambda ()
                                (when (fboundp 'auto-dim-other-buffers-mode)
                                  (auto-dim-other-buffers-mode t))))
   :ensure t)
+
+
+
+;; (use-package dimmer
+;;   :init
+;;   (setq dimmer-fraction 0.2)
+;;   ;; (setq dimmer-exclusion-regexp "eshell")
+;;   :config
+;;   (dimmer-mode)
+;;   :ensure t)
