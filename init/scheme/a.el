23696aac8c53c38a20aa36c9eeb00b036026bc75
Yanying Wang <yanyingwang1@gmail.com>
2019-03-22 11:57:12 +0800

    scheme

--- init/scheme/a.el
+++ init/scheme/a.el
@@ -1,2 +1,22 @@
-(setq scheme-program-name "chez")
+;; (setq scheme-program-name "chez")
 ;; (add-hook 'scheme-mode-hook 'geiser-mode)
+
+
+(setenv "CHEZSCHEMELIBDIRS"
+        (concat
+         (getenv "HOME")
+         "/.chezlib"))
+
+
+
+(use-package racket-mode
+  ;; :bind (("C-x g" . magit-status))
+  :init
+  ;; (add-hook 'racket-mode-hook
+  ;;           (lambda ()
+  ;;             (define-key racket-mode-map (kbd "C-c r") 'racket-run)))
+  (add-hook 'racket-mode-hook      #'racket-unicode-input-method-enable)
+  (add-hook 'racket-repl-mode-hook #'racket-unicode-input-method-enable)
+  (setq tab-always-indent 'complete)
+
+  :ensure t)
