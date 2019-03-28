c114619dfb004ed4b5eb27e2dc97a56f6e048b19
Yanying Wang <yanyingwang1@gmail.com>
2019-03-28 16:36:50 +0800

    scribble mode

---
+++ init/scheme/racket.el
@@ -0,0 +1,19 @@
+
+(use-package racket-mode
+  :init
+  (add-to-list 'auto-mode-alist '("\\.rkt?\\'" . racket-mode))
+  (add-to-list 'auto-mode-alist '("\\.*racketrc\\'" . racket-mode))
+  ;; (add-hook 'racket-mode-hook
+  ;;           (lambda ()
+  ;;             (define-key racket-mode-map (kbd "C-c r") 'racket-run)))
+  (add-hook 'racket-mode-hook      #'racket-unicode-input-method-enable)
+  (add-hook 'racket-repl-mode-hook #'racket-unicode-input-method-enable)
+  (setq tab-always-indent 'complete)
+  :ensure t)
+
+
+
+(use-package scribble-mode
+  :init
+  (add-hook 'scribble-mode-hook #'geiser-mode)
+  :ensure t)
