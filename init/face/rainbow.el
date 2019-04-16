3c13d0f7d72f49b357c1c42bb076c078034c8634
Yanying Wang <yanyingwang1@gmail.com>
2019-04-16 12:11:03 +0800

    modify delimiter colors

--- init/face/rainbow.el
+++ init/face/rainbow.el
@@ -10,17 +10,16 @@
  ;; If there is more than one, they won't work right.
  '(rainbow-delimiters-depth-1-face ((t (:foreground "#458588"))))
  '(rainbow-delimiters-depth-2-face ((t (:foreground "#b16286"))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "#8ec07c"))))
- '(rainbow-delimiters-depth-4-face ((t (:foreground "#d65d0e"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "DarkSeaGreen"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "IndianRed"))))
 
- '(rainbow-delimiters-depth-5-face ((t (:foreground "BlueViolet"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "MediumPurple")))) ;"SlateGray" "DarkOrchid"
 
- '(rainbow-delimiters-depth-6-face ((t (:foreground "dark cyan"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "SteelBlue")))) ;"DarkCyan" "SteelBlue"
  '(rainbow-delimiters-depth-7-face ((t (:foreground "DeepPink2"))))
  '(rainbow-delimiters-depth-8-face ((t (:foreground "OliveDrab"))))
  '(rainbow-delimiters-depth-9-face ((t (:foreground "dark red")))))
-
-
+;; https://www.w3schools.com/colors/colors_names.asp
 
 (use-package rainbow-mode
   :init
