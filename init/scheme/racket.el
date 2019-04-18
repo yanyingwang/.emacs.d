8b4bfe91ec39b3f8734d91ebfd844a7a237fb168
Yanying Wang <yanyingwang1@gmail.com>
2019-04-18 14:06:37 +0800

    racket

--- init/scheme/racket.el
+++ init/scheme/racket.el
@@ -3,10 +3,10 @@
   :init
   (add-to-list 'auto-mode-alist '("\\.rkt?\\'" . racket-mode))
   (add-to-list 'auto-mode-alist '("\\.*racketrc\\'" . racket-mode))
-  (add-to-list 'auto-mode-alist '("\\.*.rkt\\'" . racket-mode))
-  ;; (add-hook 'racket-mode-hook
-  ;;           (lambda ()
-  ;;             (define-key racket-mode-map (kbd "C-c r") 'racket-run)))
+  ;; (add-to-list 'auto-mode-alist '("\\.*.rkt\\'" . racket-mode))
+  (add-hook 'racket-mode-hook
+            (lambda ()
+              (define-key racket-mode-map (kbd "C-c r") 'racket-run)))
   (add-hook 'racket-mode-hook      #'racket-unicode-input-method-enable)
   (add-hook 'racket-repl-mode-hook #'racket-unicode-input-method-enable)
   (add-hook 'racket-repl-mode-hook #'racket-unicode-input-method-enable)
