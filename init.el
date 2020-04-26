fbe7a322976a132e3a080116cff9546f325a65a7
Yanying Wang <yanyingwang1@gmail.com>
2020-04-26 16:58:09 +0800

    nil

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-(require 'package)
+q(require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 ;; (add-to-list 'package-archives '("elpa-popkit" . "http://elpa.popkit.org/"))
@@ -12,7 +12,6 @@
 (setq use-package-always-ensure t)
 
 
-(load "flymake")
 ;;; load init files
 (defun load-directory (dir)
   (let ((load-it (lambda (f)
@@ -31,25 +30,3 @@
 (load-directory "~/.emacs.d/init/others")
 (load-file "~/.emacs.d/init/helm.el")
 (load-directory "~/.emacs.d/init/face")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (flymake-diagnostic-at-point flymake-shellcheck helm-flymake heml-flymake flymake-less flymake-sass flymake-jslint flymake-css flymake-easy flymake-racket flycheck-posframe flycheck-package flycheck-grammarly flycheck flycheck-pos-tip flycheck-tip flymake-ruby vue-html-mode vue-mode youdao-dictionary yaml-mode wttrin ws-butler web-mode use-package smartparens slime slim-mode scss-mode scribble-mode rvm ruby-refactor robe region-bindings-mode rainbow-delimiters racket-mode projectile-rails pinyin-search phi-search multiple-cursors micgoline markdown-mode magit indent-guide helm-themes helm-projectile helm-descbinds haml-mode gruvbox-theme golden-ratio geiser expand-region expand-line exec-path-from-shell eshell-prompt-extras esh-autosuggest emr diff-hl company-tabnine circadian chicken-scheme cal-china-x browse-at-remote bing-dict auto-package-update auto-dim-other-buffers aggressive-indent))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(rainbow-delimiters-depth-1-face ((t (:foreground "#458588"))))
- '(rainbow-delimiters-depth-2-face ((t (:foreground "#b16286"))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "DarkSeaGreen"))))
- '(rainbow-delimiters-depth-4-face ((t (:foreground "IndianRed"))))
- '(rainbow-delimiters-depth-5-face ((t (:foreground "DarkOrchid"))))
- '(rainbow-delimiters-depth-6-face ((t (:foreground "SteelBlue"))))
- '(rainbow-delimiters-depth-7-face ((t (:foreground "DeepPink2"))))
- '(rainbow-delimiters-depth-8-face ((t (:foreground "OliveDrab"))))
- '(rainbow-delimiters-depth-9-face ((t (:foreground "dark red")))))
--- init/face/powerline.el
+++ init/face/powerline.el
@@ -1,11 +1,5 @@
-(use-package micgoline
-  :config
-  (require 'micgoline)
-  (setq ns-use-srgb-colorspace nil)
-  (setq powerline-default-separator 'slant)
-  :ensure t)
 
-
+;;--;; alternate variable
 ;;alternate
 ;;arrow
 ;;arrow-fade
@@ -25,176 +19,59 @@
 ;;nil
 
 
-;;;;;; powerline
-;;(use-package powerline
-;;  :config
-;;  (require 'powerline)
-;;  (powerline-default-theme)
-;;  ;;(setq powerline-arrow-shape 'arrow)   ; the default
-;;  (setq powerline-arrow-shape 'curve)   ; give your mode-line curves
-;;  ;;(setq powerline-arrow-shape 'arrow14) ; best for small fonts
-;;  :ensure t)
-;;
+;; (use-package powerline
+;;   :config
+;;   (require 'powerline)
+;;   (powerline-default-theme)
+;;   ;;(setq powerline-arrow-shape 'arrow)   ; the default
+;;   (setq powerline-arrow-shape 'curve)   ; give your mode-line curves
+;;   ;; (setq powerline-arrow-shape 'arrow14) ; best for small fonts
+;;   :ensure t)
+
+(use-package micgoline
+  :config
+  (require 'micgoline)
+  (setq ns-use-srgb-colorspace nil)
+  (setq powerline-default-separator 'arrow) ;; nil slant arrow
+  (setq ns-use-srgb-colorspace nil)
+  :ensure t)
+
+(use-package smart-mode-line
+  :ensure t
+  ;; :after powerline
+  ;; :after micgoline
+  ;; :after smart-mode-line
+  :init
+  (setq sml/no-confirm-load-theme t)
+  (sml/setup)
+  ;; (setq sml/theme 'dark)
+  ;; (setq sml/theme 'light)
+  (setq sml/theme 'respectful)
+  :ensure t)
+
+;; (use-package telephone-line
+;;   :config
+;;   (require 'telephone-line)
+;;   (telephone-line-mode 1)
+;;   (setq telephone-line-lhs
+;;         '((evil   . (telephone-line-evil-tag-segment))
+;;           (accent . (telephone-line-vc-segment
+;;                      telephone-line-erc-modified-channels-segment
+;;                      telephone-line-process-segment))
+;;           (nil    . (telephone-line-minor-mode-segment
+;;                      telephone-line-buffer-segment))))
+;;   (setq telephone-line-rhs
+;;         '((nil    . (telephone-line-misc-info-segment))
+;;           (accent . (telephone-line-major-mode-segment))
+;;           (evil   . (telephone-line-airline-position-segment))))
+;;   :ensure t)
 
 
+;; (use-package all-the-icons
+;;   :ensure t)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-
-;; ;;; micgoline.el --- powerline mode, color schemes from microsoft and google's logo.
-
-;; ;; Copyright (C) 2016 yzprofile
-;; ;;
-;; ;; Author: yzprofile <yzprofiles@gmail.com>
-;; ;; URL: https://github.com/yzprofile/micgoline
-;; ;; Version: 1.0.0
-;; ;; Keywords: mode-line powerline theme
-;; ;; Package-Requires: ((emacs "24.3") (powerline "2.3"))
-
-;; ;; This file is not part of GNU Emacs.
-
-;; ;; This program is free software; you can redistribute it and/or modify
-;; ;; it under the terms of the GNU General Public License as published by
-;; ;; the Free Software Foundation, either version 3 of the License, or
-;; ;; (at your option) any later version.
-
-;; ;; This program is distributed in the hope that it will be useful,
-;; ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; ;; GNU General Public License for more details.
-
-;; ;; You should have received a copy of the GNU General Public License
-;; ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-;; ;;; Commentary:
-
-;; ;; micgoline is a modular mode-line library built on the powerline package,
-;; ;; designed to make it easy to build beautiful mode-lines.
-
-;; ;;; Code:
-
-;; (require 'powerline)
-
-;; (defface micgoline-pl-active-blue
-
-;;   '((t (:background "#4885ed" :foreground "#FFFFFF" :inherit mode-line)))
-;;   "P face blue."
-;;   :group 'powerline)
-
-;; (defface micgoline-pl-inactive-blue
-;;   '((t (:background "#2b2f59" :Foreground "#FFFFFF" :inherit mode-line-inactive))) ;; "#00A1F1"
-;;   "P face blue."
-;;   :group 'powerline)
-
-;; (defface micgoline-pl-active-green
-;;   '((t (:background "#3cba54" :foreground "#FFFFFF" :inherit mode-line)))
-;;   "P face green."
-;;   :group 'powerline)
-
-;; (defface micgoline-pl-inactive-green
-;;   '((t (:background  "#16441d" :foreground "#FFFFFF" :inherit mode-line-inactive))) ;; "#7CBB00"
-;;   "P face green."
-;;   :group 'powerline)
-
-;; (defface micgoline-pl-active-red
-;;   '((t (:background "#db3236" :foreground "#FFFFFF" :inherit mode-line)))
-;;   "P face red."
-;;   :group 'powerline)
-
-;; (defface micgoline-pl-inactive-red
-;;   '((t (:background "#59342a" :foreground "#FFFFFF" :inherit mode-line-inactive))) ;; "#F65314"
-;;   "P face red."
-;;   :group 'powerline)
-
-;; (defface micgoline-pl-active-yellow
-;;   '((t (:background "#f4c20d" :foreground "#FFFFFF" :inherit mode-line)))
-;;   "P face yellow."
-;;   :group 'powerline)
-
-;; (defface micgoline-pl-inactive-yellow
-;;   '((t (:background "#54592a" :foreground "#FFFFFF" :inherit mode-line-inactive))) ;; "#FFBB00"
-;;   "P face yellow."
-;;   :group 'powerline)
-
-
-;; (setq powerline-default-separator 'slant)
-
-
-;; (defun micgoline-load-theme ()
-;;   "Setup the default mode-line."
-;;   (interactive)
-;;   (setq-default
-;;    mode-line-format
-;;    '("%e"
-;;      (:eval
-;;       (let* ((active (powerline-selected-window-active))
-;;              (mode-line (if active 'mode-line 'mode-line-inactive))
-;;              (blue (if active 'micgoline-pl-active-blue 'micgoline-pl-inactive-blue))
-;;              (green (if active 'micgoline-pl-active-green 'micgoline-pl-inactive-green))
-;;              (red (if active 'micgoline-pl-active-red 'micgoline-pl-inactive-red))
-;;              (yellow (if active 'micgoline-pl-active-yellow 'micgoline-pl-inactive-yellow))
-;;              (separator-right (intern (format "powerline-%s-%s"
-;;                                               (powerline-current-separator)
-;;                                               (car powerline-default-separator-dir))))
-;;              (separator-left (intern (format "powerline-%s-%s"
-;;                                              (powerline-current-separator)
-;;                                              (cdr powerline-default-separator-dir))))
-;;              (lhs (list (powerline-raw "%*" blue 'l)
-;;                         (powerline-raw "%4l" blue 'l)
-;;                         (powerline-raw ":" blue 'l)
-;;                         (powerline-raw "%3c" blue 'r)
-
-;;                         (when powerline-display-buffer-size
-;;                           (powerline-buffer-size blue 'l))
-
-;;                         (when powerline-display-mule-info
-;;                           (powerline-raw mode-line-mule-info blue 'l))
-
-;;                         (funcall separator-left blue red)
-
-;;                         (set-face-attribute 'mode-line-buffer-id nil :foreground "#FFFFFF")
-;;                         (powerline-buffer-id red 'l)
-
-;;                         (when (and (boundp 'which-func-mode) which-func-mode)
-;;                           (powerline-raw which-func-format red 'l))
-;;                         (powerline-raw " " red)
-
-;;                         (funcall separator-right red yellow)
-
-;;                         (when (boundp 'erc-modified-channels-object)
-;;                           (powerline-raw erc-modified-channels-object nil 'l))
-
-;;                         (powerline-major-mode yellow 'l)
-;;                         (powerline-process yellow)
-;;                         (powerline-raw " " yellow)
-;;                         (funcall separator-left yellow blue)
-;;                         (powerline-minor-modes blue 'l)
-;;                         (powerline-narrow blue 'l)
-;;                         (powerline-raw " " blue)
-;;                         (funcall separator-right blue green)
-;;                         (powerline-vc green 'l)))
-
-;;              (rhs (list (powerline-raw global-mode-string green 'r)
-;;                         (funcall separator-right green red)
-;;                         (unless window-system
-;;                           (powerline-raw (char-to-string #xe0a1) red 'l))
-;;                         (powerline-raw " " red)
-;;                         (powerline-raw "%6p" red 'r)
-;;                         (when powerline-display-hud
-;;                           (powerline-hud mode-line blue)))))
-;;         (concat (powerline-render lhs)
-;;                 (powerline-fill green (powerline-width rhs))
-;;                 (powerline-render rhs)))))))
-
-;; (micgoline-load-theme)
-
-;; (provide 'micgoline)
-
-;; ;;; micgoline.el ends here
-
-
-;; (require 'micgoline)
-;; (setq ns-use-srgb-colorspace nil)
-;; (setq powerline-default-separator 'slant)
+;; (use-package doom-modeline
+;;   :ensure t
+;;   :init
+;;   (doom-modeline-mode 1)
+;;   :ensure t)
