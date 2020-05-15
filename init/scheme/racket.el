5413178f643d01bc95cdc46159edc1b59ab4f895
Yanying Wang <yanyingwang1@gmail.com>
2020-05-15 14:20:39 +0800

    racket-xp

--- init/scheme/racket.el
+++ init/scheme/racket.el
@@ -14,7 +14,7 @@
   ;; (add-hook 'racket-repl-mode-hook #'racket-unicode-input-method-enable)
   ;; (add-hook 'racket-repl-mode-hook (lambda () (setq show-trailing-whitespace nil)))
   ;; (setq tab-always-indent 'complete)
-  (add-to-list 'auto-mode-alist '("\\.plist$" . racket-mode))
+  (add-to-list 'auto-mode-alist '("\\.rkt$" . racket-xp-mode))
   (add-to-list 'auto-mode-alist '("\\.rkt?\\'" . racket-mode))
   (add-to-list 'auto-mode-alist '("\\.rkt\\'" . racket-mode))
   (add-to-list 'auto-mode-alist '("\\.*racketrc\\'" . racket-mode))
