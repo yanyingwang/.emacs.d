aff46e2924410f42335636ff7784e19616395053
Yanying Wang <yanyingwang1@gmail.com>
2022-03-15 01:22:00 +0800

    fix font theme

--- init/face/font-theme.el
+++ init/face/font-theme.el
@@ -1,33 +1,28 @@
-
-(setq-default line-spacing 2)
+;; (setq-default line-spacing 1)
 ;; (setq-default letter-spacing 2)
 
 
-;; "agave-14:bold"
-(cond ((string-equal system-type "windows-nt")
-       (progn (message "Font set for Windows")))
-      ((string-equal system-type "darwin")
-       (progn (set-default-font "monaco-14:bold")
-              ;; (set-default-font "agave-16:bold")
-              (set-frame-font "monaco-14:bold" nil t)))
-      ((string-equal system-type "gnu/linux")
-       (progn (set-default-font "MonacoB2-10:bold")
-              ;; (set-default-font "agave-16:bold")
-              (set-frame-font "MonacoB2-10:bold" nil t))))
-
-
 (use-package gruvbox-theme
-  :config
-  ;;(load-theme 'gruvbox t)
-  ;;(load-theme 'gruvbox-light-soft  t)
-  ;;(load-theme 'gruvbox-light-medium t)
-  ;;(load-theme 'gruvbox-light-hard  t)
-  ;;(load-theme 'gruvbox-dark-soft  t)
-  ;;(load-theme 'gruvbox-dark-medium t)
-  ;;(load-theme 'gruvbox-dark-hard  t)
+  :init
+  ;; (set-face-foreground 'indent-guide-face "lightgray")
+  ;; (set-face-foreground 'indent-guide-face "dimgray")
+  ;; (load-theme 'gruvbox t)
+  ;; (load-theme 'gruvbox-light-soft  t)
+  ;; (load-theme 'gruvbox-light-medium t)
+  ;; (load-theme 'gruvbox-light-hard  t)
+  (load-theme 'gruvbox-dark-soft  t)
+  ;; (load-theme 'gruvbox-dark-medium t)
+  ;; (load-theme 'gruvbox-dark-hard  t)
+
+  ;; (setq fontname "agave-12:bold")
+  (setq fontname "MonacoB2-10:bold")
+  (set-face-attribute 'default nil :font fontname)
+  (set-frame-font fontname nil t)
+  (set-face-foreground 'indent-guide-face "dimgray")
+  ;; (set-face-foreground 'indent-guide-face "lightgray")
   :ensure t :defer)
 
-;;
+
 ;;(use-package doom-themes
 ;;  :init
 ;;  (require 'doom-themes)
@@ -52,39 +47,49 @@
 ;;  :ensure t)
 ;;
 
+;; (use-package circadian
+;;   :ensure t
+;;   :config
+;;   ;;(setq circadian-themes '(("8:00" . gruvbox-light-medium)
+;;   ;;                         ("19:00" . gruvbox-dark-medium)))
+;;   (setq calendar-latitude 31.230390)
+;;   (setq calendar-longitude 121.473702)
+;;   ;; (setq circadian-themes '((:sunrise . gruvbox-dark-soft)
+;;   ;;                          (:sunset  . gruvbox-dark-hard)))
+;;   (setq circadian-themes '((:sunrise . gruvbox-light-soft)
+;;                            (:sunset  . gruvbox-light-hard)))
 
-(use-package circadian
-  :ensure t
-  :config
-  ;;(setq circadian-themes '(("8:00" . gruvbox-light-medium)
-  ;;                         ("19:00" . gruvbox-dark-medium)))
-  (setq calendar-latitude 31.230390)
-  (setq calendar-longitude 121.473702)
-  (setq circadian-themes '((:sunrise . gruvbox-dark-soft)
-                           (:sunset  . gruvbox-dark-hard)))
-  (add-hook 'circadian-after-load-theme-hook
-            #'(lambda (theme)
-                (progn
-                  ;;; set fonts with check OS type
-                  ;; "agave-11:bold"
-                  ;; "MonacoB2-10:bold"
-                ;;   (set-face-attribute 'default nil :font "monaco-14:bold")
-                ;;   (set-frame-font "MonacoB2-10:bold" nil t)
-                ;;   (set-face-foreground 'indent-guide-face "dimgray"))
-                ;; (cond
-                ;;  ((or (string-equal theme "gruvbox-light-soft")
-                ;;       (string-equal theme "gruvbox-light-medium")
-                ;;       (string-equal theme "gruvbox-light-hard"))
-                ;;   (progn
-                ;;     (set-face-attribute 'default nil :font (concat fontname ":bold"))
-                ;;     (set-frame-font (concat fontname ":book") nil t)
-                ;;     (set-face-foreground 'indent-guide-face "lightgray")))
-                ;;  ((or (string-equal theme "gruvbox-dark-soft")
-                ;;       (string-equal theme "gruvbox-dark-medium")
-                ;;       (string-equal theme "gruvbox-dark-hard"))
-                ;;   (progn
-                ;;     (set-face-attribute 'default nil :font (concat fontname ":book"))
-                ;;     (set-frame-font (concat fontname ":bold") nil t)
-                ;;     (set-face-foreground 'indent-guide-face "dimgray"))))
-                  ))
-  (circadian-setup))
+;;   (add-hook 'circadian-after-load-theme-hook
+;;             #'(lambda (theme)
+;;                 (cond
+;;                  ;; ((string-equal system-type "windows-nt")
+;;                  ;;  (progn (message "Font set for Windows")))
+;;                  ((string-equal system-type "darwin")
+;;                   (setq font-name "Monaco-10")
+;;                   ;; (setq font-name "agave-12")
+;;                   )
+;;                  ((string-equal system-type "gnu/linux")
+;;                   (font-name "agave-12")
+;;                   ;; (font-name "MonacoB-10")
+;;                   ))
+
+;;                 (cond
+;;                  ((or (string-equal theme "gruvbox-light-soft")
+;;                       (string-equal theme "gruvbox-light-medium")
+;;                       (string-equal theme "gruvbox-light-hard"))
+;;                   (progn
+;;                     (set-face-attribute 'default nil :font (concat fontname ":bold"))
+;;                     (set-frame-font (concat fontname ":bold") nil t)
+;;                     (set-face-foreground 'indent-guide-face "lightgray")))
+
+;;                  ((or (string-equal theme "gruvbox-dark-soft")
+;;                       (string-equal theme "gruvbox-dark-medium")
+;;                       (string-equal theme "gruvbox-dark-hard"))
+;;                   (progn
+;;                     (set-face-attribute 'default nil :font (concat fontname ":book"))
+;;                     (set-frame-font (concat fontname ":bold") nil t)
+;;                     (set-face-foreground 'indent-guide-face "dimgray"))))
+;;                 ))
+;;   (circadian-setup))
+
+
