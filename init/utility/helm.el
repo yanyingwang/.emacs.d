10884dd781eacafbc7d50bf9fd374fe6b1e4e2b2
Yanying Wang <yanyingwang1@gmail.com>
2023-07-14 17:37:31 +0800

    fix helm

--- init/utility/helm.el
+++ init/utility/helm.el
@@ -2,7 +2,7 @@
 ;;; helm
 (use-package helm
   :bind (("M-x" . helm-M-x)
-         ("C-x C-f" . hexl-find-file)
+         ("C-x C-f" . dired-x-find-file)
          ("C-c h" . helm-command-prefix)
          ("M-y" . helm-show-kill-ring)
          ("C-x b" . helm-mini)
